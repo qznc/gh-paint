bce70cc617415101ed45baf291b9cfcf8940083d
Andreas Zwinkau <zwinkau@mailbox.org>
2025-01-10 17:49:42 +0100

    continue

---
+++ README.md
@@ -0,0 +1,2 @@
+# Paint the GitHub stats
+
--- paint.py
+++ paint.py
@@ -1,6 +1,20 @@
 import subprocess
+from datetime import datetime, timedelta
 
 COMMIT_COUNTER = 1
+FILENAME = "dummy.txt"
+BRANCH = "gh-paint"
+YEAR = 2024
+width = 52
+height = 7
+with open("gh.data", "br") as fh:
+    data = fh.read()
+
+
+def run(cmd):
+    print(cmd)
+    cp = subprocess.run(cmd, check=True, capture_output=True)
+
 
 def create_commit(date):
     global COMMIT_COUNTER
@@ -8,67 +22,31 @@ def create_commit(date):
     with open("dummy.txt", "w") as fh:
         fh.write(f"{COMMIT_COUNTER}\n")
         COMMIT_COUNTER += 1
-    subprocess.run(['git', 'commit', '-m', f'Commit for {date}', '--date', commit_date])
+    run(["git", "add", FILENAME])
+    run(["git", "commit", "-m", f"Commit for {date}", "--date", commit_date])
 
-width = 52
-height = 7
 
-header_data = (
-    "````````````>H:W%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2```````"
-    "%2%2%2%2%2%2````````````````````%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2%2"
-    "%2%2%2%2%2%2`````````````````````"
-    "%2%2%2%2%2%2%2%2````%2%20$Q]```"
-    "%2%20$Q]%2%2````%2%2%2%2%2%2%2%2%2%2```"
-    "%2%2%2%2%2%2%2%2%2%2%2%2%2%2````%2%2%2%2%2%2%2%2%2%2```"
-    "````%2%2%2%2%2%2```````%2%2%2%2```````>H:W"
-    "%2%2```````>H:W%2%2````````````%2%2%2%2%2%2%2%2```````"
-    "%2%2```````%2%2`````````````````````"
-    "%2%2````%2%2%2%2```````````````````"
-    "%2%2```````%2%2````````0$Q]```````%2%2%2%2%2%2%2%2"
-    "%2%2`````````````%2%2```````%2%2%2%2%2%2```"
-    "%2%2%2%2%2%2%2%2%2%2````%2%2%2%2````````>H:W%2%2```````%2%2"
-    "```````%2%2```````%2%2```````%2%2%2%2```"
-    "%2%2%2%2%2%2%2%2%2%2````%2%2```````%2%2```"
-    "HJ[?0$Q]%2%2````%2%2%2%2%2%2%2%2%2%2```````%2%2```"
-    "````%2%2HJ[?HJ[?```````%2%2```````%2%2```"
-    "````%2%2%2%2```````%2%2```````"
-    "%2%2```````0$Q]%2%2```````%2%2%2%2"
-    "```````%2%2```````````````````>H:W%2%2>H:W```"
-    "%2%2````%2%2%2%2```````%2%2%2%2%2%2%2%2%2%2```````"
-    "%2%2%2%2%2%2>H:W`````````````%2%2%2%2%2%2%2%2"
-    "%2%2```````%2%2```````%2%2````````"
-)
+def commit_dates():
+    current_year = datetime.now().year
+    first_day_of_last_year = datetime(current_year - 1, 1, 1)
+    cur = first_day_of_last_year
+    while cur.year < current_year:
+        yield cur
+        cur += timedelta(days=1)
 
-with open("gh.data", "br") as fh:
-    data = fh.read()
 
-for y in range(height):
-    for x in range(width):
-        pixel = data[y*width + x]
-        print(f"{pixel:x} ", end="")
-    print()
-
-from datetime import datetime, timedelta
-
-def date_range_past_year():
-    today = datetime.now().date()
-    start_date = today - timedelta(days=52*7)
-    
-    current_date = start_date
-    while current_date <= today:
-        yield current_date
-        current_date += timedelta(days=1)
-
-def handle_date(i, date):
+def do_date(i, date):
     x = i // 7
     y = i % 7
-    pixel = data[y*width + x]
+    pixel = data[y * width + x] // 16
     for i in range(pixel):
         create_commit(date)
 
-print()
+
+run(["git", "checkout", "-b", BRANCH])
 i = -1
-for date in date_range_past_year():
+for date in commit_dates():
     if i >= 0 or date.isoweekday() == 7:
         i += 1
-        handle_date(i, date)
+        do_date(i, date)
+        # break # DEV
